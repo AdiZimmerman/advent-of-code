96c1490f1f002984299b088956a66705f20a484f
Adi Zimmerman <adizimmerman1@gmail.com>
2024-09-15 20:10:56 -0700

    fix main

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -1,14 +1,7 @@
 import argparse
 from typing import Optional
 
-def find_digit(line: str, pos: int) -> Optional[int]:
-	"""Returns the digit representation of word from position in line or None if no valid word found."""
-	for i, word in enumerate(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
-		if line[pos:pos+len(word)] == word:
-			return i + 1
-	return None
-
-def part(filename: str, func) -> int:
+def solution(filename: str, func) -> int:
 	res: int = 0
 	with open(filename) as f:
 		for line in f:
@@ -23,14 +16,23 @@ def part(filename: str, func) -> int:
 		return res
 
 def part_one(filename: str) -> int:
-	return part(filename, lambda line, i: None)
+	return solution(filename, lambda line, i: None)
 
 def part_two(filename: str) -> int:
-	return part(filename, lambda line, i: find_digit(line, i))
+	def find_digit(line: str, pos: int) -> Optional[int]:
+		for i, word in enumerate(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+			if line[pos:pos+len(word)] == word:
+				return i + 1
+		return None
+	return solution(filename, lambda line, i: find_digit(line, i))
 
 def test(f, filename, expected):
 	actual = f(filename)
 	assert expected == actual, f"{f, filename}: expected {expected}, actual {actual}"
 
 if __name__ == "__main__":
-	print(part_one('input.txt'))
+	test(part_one, 'test1.txt', 142)
+	test(part_one, 'input.txt', 54940)
+	test(part_two, 'test1.txt', 142)
+	test(part_two, 'test2.txt', 281)
+	test(part_two, 'input.txt', 54208)

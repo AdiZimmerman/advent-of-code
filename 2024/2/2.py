5ed486a18082b32895f59f8e5a4ce04c5e776057
Adi Zimmerman <adizimmerman1@gmail.com>
2024-12-18 17:46:24 -0800

    clean

--- 2024/2/2.py
+++ 2024/2/2.py
@@ -9,13 +9,12 @@ def part_two():
 	ans = 0
 	with open('input.txt') as f:
 		for line in f:
-			ans += safe(line.split(' '),True)
+			ans += safe(line.split(' '), True)
 	return ans
 
 def safe(reports, remove=False):
 	inc = 0
 	i = 0
-	errors = 0
 	while i < len(reports)-1:
 		cur, nxt = int(reports[i]), int(reports[i+1])
 		diff = cur - nxt
@@ -23,7 +22,7 @@ def safe(reports, remove=False):
 		differ = abs(diff) < 1 or abs(diff) > 3
 		if wrong_way or differ:
 			if remove:
-				return safe(reports[1:],False) or safe(reports[:i]+reports[i+1:],False) or safe(reports[:i+1]+reports[i+2:],False)
+				return safe(reports[1:]) or safe(reports[:i]+reports[i+1:]) or safe(reports[:i+1]+reports[i+2:])
 			return 0
 		inc = -1 if diff > 0 else 1
 		i += 1
